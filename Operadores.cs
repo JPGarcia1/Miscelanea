2a5dd683713a055d51a92df5ec84e88fac09576b
Jean Paul <jpgarcia884@misena.edu.co>
2022-05-03 17:21:51 -0500

    ~

--- Operadores.cs
+++ Operadores.cs
@@ -15,7 +15,6 @@ namespace LA_MISCELANEA_C
             do
             {
 
-
                 Console.WriteLine("SELECCIONE LA OPCIÓN DESEADA");
                 Console.WriteLine(" 1. Area de un triángulo");
                 Console.WriteLine(" 2. Suma de dos números");
@@ -27,8 +26,8 @@ namespace LA_MISCELANEA_C
                 Console.WriteLine(" 8. Promedio de tres numeros");
                 Console.WriteLine(" 9. SALIR");
 
-                ElecOp = Convert.ToInt32(Console.ReadLine());
 
+                ElecOp = Convert.ToInt32(Console.ReadLine());
 
                 switch (ElecOp)
                 {
@@ -90,89 +89,153 @@ namespace LA_MISCELANEA_C
 
         static void triangulo()
         {
-            double resul;
-            Console.WriteLine("Ingrese la base del triangulo");
-            double bas = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese la altura del triangulo");
-            double alt = Convert.ToDouble(Console.ReadLine());
-            resul = (bas * alt) / 2;
-            Console.WriteLine("El resultado es:" + resul);
+            try
+            {
+                double resul;
+                Console.WriteLine("Ingrese la base del triangulo");
+                double bas = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese la altura del triangulo");
+                double alt = Convert.ToDouble(Console.ReadLine());
+                resul = (bas * alt) / 2;
+                Console.WriteLine("El resultado es:" + resul);
+            }
+            catch (Exception tri)
+            {
+                Console.WriteLine("Error en el dato ingresado");
+                Console.WriteLine(tri.Message);
+            }
         }
 
         static void suma()
         {
-            double resul;
-            Console.WriteLine("Ingrese el primer numero");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el segundo numero");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            resul = num1 + num2;
-            Console.WriteLine("El resultado es:" + resul);
+            try
+            {
+                double resul;
+                Console.WriteLine("Ingrese el primer numero");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo numero");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                resul = num1 + num2;
+                Console.WriteLine("El resultado es:" + resul);
+            }
+            catch(Exception suma)
+            {
+                Console.WriteLine();
+                Console.WriteLine(suma.Message);
+            }
         }
 
         static void cuadrado()
         {
-            double resul;
-            Console.WriteLine("Ingrese el numero");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            resul = num1 * num1;
-            Console.WriteLine("El resultado es:" + resul);
-        }
-
+            try
+            {
+                double resul;
+                Console.WriteLine("Ingrese el numero");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                resul = num1 * num1;
+                Console.WriteLine("El resultado es:" + resul);
+            }
+            catch (Exception cuadra)
+            {
+                Console.WriteLine();
+                Console.WriteLine(cuadra.Message);
+            }
+        }    
+        
         static void conversion()
         {
-            double resul;
-            Console.WriteLine("Ingrese el numero de euros que desea convertir");
-            double valor1 = Convert.ToDouble(Console.ReadLine());
-            resul = valor1 * 0.94;
-            Console.WriteLine("Euros = €" + valor1 + " Equivalen a $" + resul + " Dolares");
+            try
+            {
+                double resul;
+                Console.WriteLine("Ingrese el numero de euros que desea convertir");
+                double valor1 = Convert.ToDouble(Console.ReadLine());
+                resul = valor1 * 0.94;
+                Console.WriteLine("Euros = €" + valor1 + " Equivalen a $" + resul + " Dolares");
+            }
+            catch(Exception conver)
+            {
+                Console.WriteLine();
+                Console.WriteLine(conver.Message);
+            }
         }
 
         static void lados()
         {
-            double resul1, resul2;
-            Console.WriteLine("Ingrese un lado del cuadrado");
-            double lad = Convert.ToDouble(Console.ReadLine());
-            resul1 = Math.Pow(lad, 2);
-            resul2 = lad * 4;
-            Console.WriteLine("El area del cuadrado es: " + resul1);
-            Console.WriteLine("El perimetro del cuadrado es: " + resul2);
+            try
+            {
+                double resul1, resul2;
+                Console.WriteLine("Ingrese un lado del cuadrado");
+                double lad = Convert.ToDouble(Console.ReadLine());
+                resul1 = Math.Pow(lad, 2);
+                resul2 = lad * 4;
+                Console.WriteLine("El area del cuadrado es: " + resul1);
+                Console.WriteLine("El perimetro del cuadrado es: " + resul2);
+            }
+            catch(Exception lad)
+            {
+                Console.WriteLine();
+                Console.WriteLine(lad.Message);
+            }
         }
 
         static void radio()
         {
-            double area, vol;
-            Console.WriteLine("Ingrese el radio del cilindro");
-            double rad = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese la altura del cilindro");
-            double altura = Convert.ToDouble(Console.ReadLine());
-            vol = Math.PI * Math.Pow(rad, 2) * altura;
-            area = (2 * (Math.PI) * rad * altura) + (2 * (Math.PI) * Math.Pow(rad, 2));
-            Console.WriteLine("El volumen del cilindro es: " + vol);
-            Console.WriteLine("El area del cilindro es: " + area);
+            try
+            {
+                double area, vol;
+                Console.WriteLine("Ingrese el radio del cilindro");
+                double rad = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese la altura del cilindro");
+                double altura = Convert.ToDouble(Console.ReadLine());
+                vol = Math.PI * Math.Pow(rad, 2) * altura;
+                area = (2 * (Math.PI) * rad * altura) + (2 * (Math.PI) * Math.Pow(rad, 2));
+                Console.WriteLine("El volumen del cilindro es: " + vol);
+                Console.WriteLine("El area del cilindro es: " + area);
+            }
+            catch(Exception rad)
+            {
+                Console.WriteLine();
+                Console.WriteLine(rad.Message);
+            }
         }
 
         static void circunferencia()
         {
-            Console.WriteLine("Ingrese el radio del circulo");
-            double radio = Convert.ToDouble(Console.ReadLine());
-            double circu = 2 * Math.PI * radio;
-            double area = Math.PI * Math.Pow(radio, 2);
-            Console.WriteLine("La longitud de la circunferencia es: " + circu);
-            Console.WriteLine("El area del circulo es: " + area);
+            try
+            {
+                Console.WriteLine("Ingrese el radio del circulo");
+                double radio = Convert.ToDouble(Console.ReadLine());
+                double circu = 2 * Math.PI * radio;
+                double area = Math.PI * Math.Pow(radio, 2);
+                Console.WriteLine("La longitud de la circunferencia es: " + circu);
+                Console.WriteLine("El area del circulo es: " + area);
+            }
+            catch (Exception cir)
+            {
+                Console.WriteLine();
+                Console.WriteLine(cir.Message);
+            }
         }
 
         static void promedio()
         {
-            double resul1;
-            Console.WriteLine("Ingrese el primer numero");
-            double numero1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el segundo numero");
-            double numero2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el tercer numero");
-            double numero3 = Convert.ToDouble(Console.ReadLine());
-            resul1 = (numero1 + numero2 + numero3) / 3;
-            Console.WriteLine("El promedio de los tres numeros es: " + resul1);
+            try
+            {
+                double resul1;
+                Console.WriteLine("Ingrese el primer numero");
+                double numero1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo numero");
+                double numero2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese el tercer numero");
+                double numero3 = Convert.ToDouble(Console.ReadLine());
+                resul1 = (numero1 + numero2 + numero3) / 3;
+                Console.WriteLine("El promedio de los tres numeros es: " + resul1);
+            }
+            catch(Exception pro)
+            {
+                Console.WriteLine();
+                Console.WriteLine(pro.Message);
+            }
         }
         Operadores op = new Operadores();
     }
