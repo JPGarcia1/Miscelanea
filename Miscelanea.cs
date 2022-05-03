cd519d731594bcc2bdce2b3e25f97c70c6392cc9
Jean Paul <jpgarcia884@misena.edu.co>
2022-05-03 16:44:14 -0500

    6

---
+++ Miscelanea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelanea
+{
+    internal class Miscelanea
+    {
+        public static void miscelanea()
+        {
+
+        }
+    }
+}
